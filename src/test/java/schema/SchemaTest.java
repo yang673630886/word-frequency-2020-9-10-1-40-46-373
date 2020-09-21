f0233880de1b70366d61cd8b3b3d264c4397bf4b
yangjingsheng <673630886@qq.com>
2020-09-21 17:38:01 +0800

    should_return_boolean_type_when_given_flag 测试失败

--- src/test/java/schema/SchemaTest.java
+++ src/test/java/schema/SchemaTest.java
@@ -1,4 +1,21 @@
 package schema;
 
+import com.thoughtworks.basic.schema.FlagsSchema;
+import com.thoughtworks.basic.schema.Schema;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class SchemaTest {
+    @Test
+    public void should_return_boolean_type_when_given_flag(){
+        Set<FlagsSchema> flagSchema = new HashSet<>();
+        flagSchema.add(new FlagsSchema("l",Boolean.TYPE));
+        Schema schema = new Schema(flagSchema);
+
+        Assert.assertEquals(schema.getValueOf("l"),Boolean.TYPE);
+    }
+
 }
